cf57a17b78d9e9f92797d6499f9ca85cf6e99344
Krzysztof Gołuchowski <kgoluchowski@student.agh.edu.pl>
2023-12-29 23:15:04 +0100

    Fix movement of Animals

--- PO_Projekt/src/main/java/org/model/Main.java
+++ PO_Projekt/src/main/java/org/model/Main.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Board map = new Board(40, 20);
+        Board map = new Board(5, 5);
 
-        List<Integer> genotype = List.of(7,7,7,7,7);
+        List<Integer> genotype = List.of(2, 0, 2, 0, 2);
         genotype = new ArrayList<>(genotype);
-        List<Integer> genotype1 = List.of(0, 0, 2, 0, 0);
-        genotype1 = new ArrayList<>(genotype1);
-        SimulationParameters simulationParameters = new SimulationParameters(10, 5, 5, 0, 0, SimulationParameters.MutationVariant.RANDOM, 2, 2, 1, 5);
+//        List<Integer> genotype1 = List.of(0, 0, 2, 0, 0);
+//        genotype1 = new ArrayList<>(genotype1);
+        SimulationParameters simulationParameters = new SimulationParameters(2, 5, 5, 0, 0, SimulationParameters.MutationVariant.RANDOM, 2, 2, 1, 5);
 
 
 
-        Animal chomik = new Animal(new Vector2D(12, 4), 10, genotype, simulationParameters);
-        Animal chomik1 = new Animal(new Vector2D(11, 4), 10, genotype1, simulationParameters);
+        Animal chomik = new Animal(new Vector2D(2, 2), 10, genotype, simulationParameters);
+//        Animal chomik1 = new Animal(new Vector2D(11, 4), 10, genotype1, simulationParameters);
 
-        Simulation simulation = new Simulation(simulationParameters, map, List.of(chomik, chomik1));
+        Simulation simulation = new Simulation(simulationParameters, map, List.of(chomik));
         simulation.run();
     }
 }
